e3238facedb5ea0b22438a9b73a03305d37d6797
pikkauhn <114180449+pikkauhn@users.noreply.github.com>
2023-05-30 14:23:48 -0500

    Server side started

---
+++ server/src/router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const isLoggedIn = require('./middleware/isLoggedIn');
+const updateTodosRoute = require('./routes/updateTodoRoute');
+const readTodosRoute = require('./routes/readTodosRoute');
+const createTodosRoute = require('./routes/createTodosRoute');
+const deleteTodoRoute = require('./routes/deleteTodoRoute');
+
+const router = express.Router();
+
+
+router.post('/login', require('./routes/loginRoute'));
+
+router.get('/todos', isLoggedIn, readTodosRoute);
+router.post('/todos', isLoggedIn, createTodosRoute);
+router.put('/todos/:id', isLoggedIn, updateTodosRoute);
+router.delete('/todos/:id', isLoggedIn, deleteTodoRoute);
+// delete
+
+module.exports = router;
